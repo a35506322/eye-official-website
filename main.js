af5c606b23672e8a994151f6fe7354d6eaa3e0a2
chunglijung <andy28572463@gmail.com>
2023-08-03 23:15:37 +0800

    feat: 新增環境變數讓router 可以順利在各環境執行
    %VITE_SERVER_URL% 可以用於在HTML 打環境變數

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
-import './assets/scss/all.scss';
+import "./assets/scss/all.scss";
 
 console.log("Hello world!");
+
+console.log("env:" + import.meta.env.VITE_MODE);
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite';
-import { ViteEjsPlugin } from 'vite-plugin-ejs';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-import { glob } from 'glob';
+import { defineConfig, loadEnv } from "vite";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
+import { fileURLToPath } from "node:url";
+import path from "node:path";
+import { glob } from "glob";
 
-import liveReload from 'vite-plugin-live-reload';
+import liveReload from "vite-plugin-live-reload";
 
 function moveOutputPlugin() {
   return {
-    name: 'move-output',
-    enforce: 'post',
-    apply: 'build',
+    name: "move-output",
+    enforce: "post",
+    apply: "build",
     async generateBundle(options, bundle) {
       for (const fileName in bundle) {
-        if (fileName.startsWith('pages/')) {
-          const newFileName = fileName.slice('pages/'.length);
+        if (fileName.startsWith("pages/")) {
+          const newFileName = fileName.slice("pages/".length);
           bundle[fileName].fileName = newFileName;
         }
       }
@@ -22,30 +22,40 @@ function moveOutputPlugin() {
   };
 }
 
-export default defineConfig({
-  // base 的寫法：
-  // base: '/Repository 的名稱/'
-  base: '/web-layout-training-vite/',
-  plugins: [
-    liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
-    ViteEjsPlugin(),
-    moveOutputPlugin(),
-  ],
-  server: {
-    // 啟動 server 時預設開啟的頁面
-    open: 'pages/index.html',
-  },
-  build: {
-    rollupOptions: {
-      input: Object.fromEntries(
-        glob
-          .sync('pages/**/*.html')
-          .map((file) => [
-            path.relative('pages', file.slice(0, file.length - path.extname(file).length)),
-            fileURLToPath(new URL(file, import.meta.url)),
-          ])
-      ),
+export default defineConfig(({ command, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    // base 的寫法：
+    // base: '/Repository 的名稱/'
+    base: env.VITE_BASE_URL,
+    plugins: [
+      liveReload([
+        "./layout/**/*.ejs",
+        "./pages/**/*.ejs",
+        "./pages/**/*.html",
+      ]),
+      ViteEjsPlugin(),
+      moveOutputPlugin(),
+    ],
+    server: {
+      // 啟動 server 時預設開啟的頁面
+      open: env.VITE_SERVER_URL + "index.html",
     },
-    outDir: 'dist',
-  },
+    build: {
+      rollupOptions: {
+        input: Object.fromEntries(
+          glob
+            .sync("pages/**/*.html")
+            .map((file) => [
+              path.relative(
+                "pages",
+                file.slice(0, file.length - path.extname(file).length)
+              ),
+              fileURLToPath(new URL(file, import.meta.url)),
+            ])
+        ),
+      },
+      outDir: "dist",
+    },
+  };
 });
